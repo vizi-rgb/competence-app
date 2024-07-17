952b2c03aacd992f81b47206fad5b2b3ac392004
Wiktor Nasierowski <wiktor.nasierowski@itsquad.pl>
2024-07-17 10:09:14 +0200

    Deleted initial angular layout

--- competence-fe/src/app/app.component.ts
+++ competence-fe/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {EmployeeComponent} from "./employee/employee.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
