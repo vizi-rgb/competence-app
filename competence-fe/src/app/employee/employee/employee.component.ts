08cb9b544000b013b928ba9b2925a5cc9e0b5474
Wiktor Nasierowski <wiktor.nasierowski@itsquad.pl>
2024-07-23 09:55:55 +0200

    Employee component uses reactive forms approach to update the employee's info

--- competence-fe/src/app/employee/employee/employee.component.ts
+++ competence-fe/src/app/employee/employee/employee.component.ts
@@ -1,29 +1,113 @@
 import { Component, Input } from '@angular/core';
 import { EmployeeModel } from '../../models/employee.model';
-import { DatePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { isValidDate } from 'rxjs/internal/util/isDate';
+import { DatePipe, formatDate } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { EmployeeProjectComponent } from '../employee-project/employee-project.component';
 import { MANAGERS } from '../../mocks/managers.mock';
+import { EmployeeDetailsForm } from '../../forms/employee-details.form';
+import { Technology } from '../../constants/technology.enum';
+import { SoftSkill } from '../../constants/soft-skill.enum';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [DatePipe, FormsModule, EmployeeProjectComponent],
+  imports: [
+    DatePipe,
+    FormsModule,
+    EmployeeProjectComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.scss',
 })
 export class EmployeeComponent {
   @Input()
-  employee?: EmployeeModel;
+  get employee() {
+    return this._employee;
+  }
 
+  set employee(employee: EmployeeModel | undefined) {
+    if (!employee) return;
+
+    this._employee = employee;
+    this.buildForm(employee);
+    this.newSkills = [];
+  }
+
+  private _employee?: EmployeeModel;
   managers: EmployeeModel[] = MANAGERS;
+  employeeForm: FormGroup<EmployeeDetailsForm>;
+  newSkills: (Technology | SoftSkill)[] = [];
 
-  onDateOfEmploymentChange(date: string) {
-    const newDate: Date = new Date(date);
+  constructor(private fb: FormBuilder) {
+    this.employeeForm = this.fb.nonNullable.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      dateOfEmployment: [
+        formatDate(Date.now(), 'yyyy-MM-dd', 'en'),
+        Validators.required,
+      ],
+      manager: [null as EmployeeModel | null, Validators.required],
+      newSkill: null as Technology | SoftSkill | null,
+    });
 
-    if (isValidDate(newDate) && this.employee) {
-      this.employee.dateOfEmployment = newDate;
-    }
+    this.employeeForm.controls.newSkill.valueChanges.subscribe((value) => {
+      for (const [key, techValue] of Object.entries(Technology)) {
+        if (techValue === value) {
+          const selectedTechnology = Technology[key as keyof typeof Technology];
+          this.newSkills.push(selectedTechnology);
+        }
+      }
+    });
+  }
+
+  getAvailableSkills(): (Technology | SoftSkill)[] {
+    const allSkills = Array.of<Technology | SoftSkill>(
+      ...Object.values(Technology),
+      ...Object.values(SoftSkill)
+    );
+
+    const employeeSkills = new Set<Technology | SoftSkill>(
+      this.employee?.skills ?? []
+    );
+
+    return allSkills.filter((skill) => !employeeSkills.has(skill));
+  }
+
+  onFormSubmit(): void {
+    if (this.employeeForm.invalid || !this.employee) return;
+    this.updateEmployee(this.employee, this.employeeForm);
+    this.newSkills = [];
+  }
+
+  private buildForm(employee: EmployeeModel): void {
+    this.employeeForm.setValue({
+      name: employee.name,
+      surname: employee.surname,
+      dateOfEmployment: formatDate(
+        employee.dateOfEmployment,
+        'yyyy-MM-dd',
+        'en'
+      ),
+      manager: employee.manager,
+      newSkill: null,
+    });
+  }
+
+  updateEmployee(
+    employee: EmployeeModel,
+    employeeForm: FormGroup<EmployeeDetailsForm>
+  ): void {
+    employee.name = employeeForm.value.name!;
+    employee.surname = employeeForm.value.surname!;
+    employee.dateOfEmployment = new Date(employeeForm.value.dateOfEmployment!);
+    employee.manager = employeeForm.value.manager!;
+    employee.skills = employee.skills.concat(this.newSkills);
   }
 }
---
+++ competence-fe/src/app/forms/employee-details.form.ts
@@ -0,0 +1,12 @@
+import { FormControl } from '@angular/forms';
+import { EmployeeModel } from '../models/employee.model';
+import { Technology } from '../constants/technology.enum';
+import { SoftSkill } from '../constants/soft-skill.enum';
+
+export interface EmployeeDetailsForm {
+  name: FormControl<string>;
+  surname: FormControl<string>;
+  dateOfEmployment: FormControl<string>;
+  manager: FormControl<EmployeeModel | null>;
+  newSkill: FormControl<Technology | SoftSkill | null>;
+}
