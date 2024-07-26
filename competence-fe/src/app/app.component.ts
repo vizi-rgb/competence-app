c56fad0788207ed0ab9c9abeabe4b48ba1620a73
Wiktor Nasierowski <wiktor.nasierowski@itsquad.pl>
2024-07-26 16:42:30 +0200

    Translations added

--- competence-fe/src/app/app.component.ts
+++ competence-fe/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeListComponent } from './features/employee/components/employee-list/employee-list.component';
+import { TranslateService } from '@ngx-translate/core';
+import { HeaderComponent } from './core/layout/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EmployeeListComponent],
+  imports: [RouterOutlet, EmployeeListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['pl', 'en']);
+    this.translate.use(this.translate.defaultLang);
+  }
+}
